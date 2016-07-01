13db6994f8d76cca05252d6f2e0b215cdb949776
GFV450 <gfv450@gmail.com>
2016-07-01 09:44:01 -0700

    Makestagram
    A Swift clone of the Instagram App with Parse implemented

---
+++ Makestagram/Timeline/PostTableViewCell.swift
@@ -0,0 +1,72 @@
+//
+//  PostTableViewCell.swift
+//  Makestagram
+//
+//  Created by Gian Vitola on 6/29/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Bond
+import Parse
+
+class PostTableViewCell: UITableViewCell
+{
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likesIconImageView: UIImageView!
+    @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var moreButton: UIButton!
+    
+    
+    var postDisposable: DisposableType?
+    var likeDisposable: DisposableType?
+    
+    var post: Post? {
+        didSet {
+            // 1
+            postDisposable?.dispose()
+            likeDisposable?.dispose()
+            
+            if let post = post {
+                // 2
+                postDisposable = post.image.bindTo(postImageView.bnd_image)
+                likeDisposable = post.likes.observe { (value: [PFUser]?) -> () in
+                    // 3
+                    if let value = value {
+                        // 4
+                        self.likesLabel.text = self.stringFromUserList(value)
+                        // 5
+                        self.likeButton.selected = value.contains(PFUser.currentUser()!)
+                        // 6
+                        self.likesIconImageView.hidden = (value.count == 0)
+                    } else {
+                        // 7
+                        self.likesLabel.text = ""
+                        self.likeButton.selected = false
+                        self.likesIconImageView.hidden = true
+                    }
+                }
+            }
+        }
+    }
+    
+    func stringFromUserList(userList: [PFUser]) -> String {
+        // 1
+        let usernameList = userList.map { user in user.username! }
+        // 2
+        let commaSeparatedUserList = usernameList.joinWithSeparator(", ")
+        
+        return commaSeparatedUserList
+    }
+    
+    @IBAction func moreButtonTapped(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func likeButtonTapped(sender: AnyObject)
+    {
+        post?.toggleLikePost(PFUser.currentUser()!)
+    }
+}
